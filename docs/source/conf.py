4fb88e7160d138b2ab2c631e7bde445d81b4c65f
Narein Rao <jabbawock003@gmail.com>
2026-05-06 22:03:06 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,9 +90,10 @@ html_theme_options = {
 }
 
 # Optional: add a logo image if you have one
-# html_logo = "_static/renoir_logo.png"
+html_logo = "_static/renoir_logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['custom.css']
